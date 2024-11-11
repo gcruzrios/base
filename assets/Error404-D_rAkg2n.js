3ac704f0901c12403baafaf140ac8b716b98f7f1
Greivin Cruz Ríos <gcruz@geotecnologias.com>
2024-11-11 13:37:49 -0600

    Updates

---
+++ assets/Error404-D_rAkg2n.js
@@ -0,0 +1,1 @@
+import{j as t,S as e}from"./index-CqwZHqvG.js";import{T as r}from"./Typography-D_pw9rI4.js";const a=()=>t.jsxs(e,{direction:"column",alignItems:"center",justifyContent:"center",children:[t.jsx(r,{mt:3,variant:"h1",color:"text.disabled",children:"404"}),t.jsx(r,{variant:"h6",children:"Page not found!"})]});export{a as default};
--- assets/Error404-vwT4YmJI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,S as e}from"./index-CqndXdwy.js";import{T as r}from"./Typography-DIcygPPE.js";const a=()=>t.jsxs(e,{direction:"column",alignItems:"center",justifyContent:"center",children:[t.jsx(r,{mt:3,variant:"h1",color:"text.disabled",children:"404"}),t.jsx(r,{variant:"h6",children:"Page not found!"})]});export{a as default};
